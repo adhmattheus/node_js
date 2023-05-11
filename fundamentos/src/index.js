dbb62c6ae4312f138480babf3bbcac5504aedab0
adhmattheus <mattheusadhonnaysf@hotmail.com>
2023-05-11 16:40:39 -0300

    validando cpf

--- fundamentos/src/index.js
+++ fundamentos/src/index.js
@@ -2,19 +2,25 @@ const express = require('express');
 const { v4: uuidv4 } = require('uuid');
 
 const app = express();
-app.use(express.json());
-
 const customers = [];
+app.use(express.json());
 
 
 app.post("/account", (req, res) => {
 	const { cpf, name } = req.body;
-	const id = uuidv4();
+
+	const customerAlreadyExist = customers.some(
+		(customer) => customer.cpf === cpf
+	);
+
+	if (customerAlreadyExist) {
+		return res.status(400).json({ error: "Customer already exists!" });
+	}
 
 	customers.push({
 		cpf,
 		name,
-		id,
+		id: uuidv4(),
 		statement: []
 	});
 	return res.status(201).send();
